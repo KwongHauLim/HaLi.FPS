fb5cc73776da0bce2b23375415d3184be9d2ce89
Kwong Hau Lim <haulim.kwong@gmail.com>
2021-01-22 01:05:22 +0800

    Library code

---
+++ HaLi.FPS/HaLi.FPS/Builder.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+
+namespace HaLi.FPS
+{
+    /// <summary>
+    /// For generate FPS QR code value
+    /// </summary>
+    public class Builder
+    {
+        private static string DataObject(string id, string value, int maxLength = 0)
+        {
+            if (maxLength > 0 && value.Length > maxLength)
+                value = value.Substring(0, maxLength);
+
+            id = id.PadLeft(2, '0');
+            var len = value.Length.ToString().PadLeft(2, '0');
+            return $"{id}{len}{value}";
+        }
+
+        private static string DataGroup(string id, params string[] objs)
+        {
+            return DataObject(id, string.Join("", objs));
+        }
+
+        /// <summary>
+        /// Generate FPS QR code value, coding base on https://github.com/nessgor/fps-hk-qrcode/blob/master/js/emv-code.js
+        /// </summary>
+        public static string GetCode(FPS fps)
+        {
+            var sb = new StringBuilder();
+
+            // EMV Interpreted Data
+            // Each data object is made up of three individual fields
+            // ID / Length/ Value combination
+
+            // Fixed, "01" for FPS
+            sb.Append(DataObject("00", "01"));
+            // FPS QR Code type, default "12" to generate new code
+            sb.Append(DataObject("01", fps.InitiationMethod == FPS.InitiationType.Dynamic ? "12" : "11"));
+
+            // FPS Account information
+            string operatorDomain = DataObject("00", "hk.com.hkicl");
+            string fpsAccount = string.Empty;
+            switch (fps.Identification)
+            {
+                case FPS.IdentifyMethod.FPS_ID:
+                    fpsAccount = DataObject("02", fps.FpsID, 34);
+                    break;
+                case FPS.IdentifyMethod.MobileNumber:
+                    fpsAccount = DataObject("01", fps.BankCode) + DataObject("03", fps.FpsMobile, 13);
+                    break;
+                case FPS.IdentifyMethod.EmailAddress:
+                    fpsAccount = DataObject("01", fps.BankCode) + DataObject("04", fps.FpsEmail, 34);
+                    break;
+                default:
+                    break;
+            }
+            string amountEditable = (fps.TransactionAmountEditable && fps.TransactionAmount.CompareTo(0M) > 0) ? DataObject("06", "1") : string.Empty;
+            sb.Append(DataGroup("26", operatorDomain, fpsAccount, amountEditable));
+
+            string categoryCode = DataObject("52", fps.MerchantCategoryCode, 4);
+            string merchantCountry = DataObject("58", fps.CountryCode, 2);
+            string merchantName = DataObject("59", fps.MerchantName, 25);
+            string merchantCity = DataObject("60", fps.MerchantCity, 15);
+            string transactionCurrency = DataObject("53", fps.Currency, 3);
+            string transactionAmount = fps.TransactionAmount.CompareTo(0M) <= 0 ? string.Empty : DataObject("54", fps.TransactionAmount.ToString("0.00"));
+            string transactionReference = DataObject("05", fps.ReferenceLabel, 25);
+            string additionalData = DataObject("62", transactionReference);
+
+            sb.Append(categoryCode);
+            sb.Append(transactionCurrency);
+            sb.Append(merchantCountry);
+            sb.Append(merchantName);
+            sb.Append(merchantCity);
+            sb.Append(transactionAmount);
+            sb.Append(additionalData);
+
+            // CRC
+            sb.Append("6304"); // CRC include these 4 chars
+            sb.Append(new CRC().Calc16(sb.ToString()));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ HaLi.FPS/HaLi.FPS/FPS.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaLi.FPS
+{
+    /// <summary>
+    /// (Hong Kong) Faster Payments System 轉數快
+    /// </summary>
+    public class FPS
+    {
+        /// <summary>
+        /// Should be fixed, HKICL is provider/operator of fps in hong kong
+        /// </summary>
+        public string OperatorDomain { get; set; } = "hk.com.hkicl";
+
+        /// <summary>
+        /// Payee identify include in qr code
+        /// </summary>
+        public enum IdentifyMethod
+        {
+            /// <summary>
+            /// FPS(轉數快) Payee Identifier, should be 7 Alphanumeric, and future will change 10 Alphanumeric
+            /// </summary>
+            FPS_ID,
+            /// <summary>
+            /// Payee mobile number, * seems need bank support
+            /// </summary>
+            MobileNumber,
+            /// <summary>
+            /// Payee email address, * seems need bank support
+            /// </summary>
+            EmailAddress
+        }
+        /// <summary>
+        /// Identify method
+        /// </summary>
+        public IdentifyMethod Identification { get; set; } = IdentifyMethod.FPS_ID;
+
+        /// <summary>
+        /// Payee FPS ID
+        /// </summary>
+        public string FpsID { get; set; }
+        /// <summary>
+        /// Bank Code which HK FPS supported, default "004" is HSBC 
+        /// No use when use FpsID
+        /// <see cref="https://www.hkicl.com.hk/eng/information_centre/clearing_code_and_branch_code_list.php">list here</see>
+        /// </summary>
+        public string BankCode { get; set; } = "004";
+        /// <summary>
+        /// Payee FPS Mobile Number, Format should be +852-xxxxxxxx
+        /// No use when use FpsID
+        /// </summary>
+        public string FpsMobile { get; set; }
+        /// <summary>
+        /// Payee FPS Email address
+        /// No use when use FpsID
+        /// </summary>
+        public string FpsEmail { get; set; }
+
+        /// <summary>
+        /// Document say not used in HK FPS, default "0000"
+        /// </summary>
+        public string MerchantCategoryCode { get; set; } = "0000";
+        /// <summary>
+        /// Document say not used in HK FPS, default "HK"
+        /// </summary>
+        public string CountryCode { get; set; } = "HK";
+        /// <summary>
+        /// Document say not used in HK FPS, default "NA"
+        /// </summary>
+        public string MerchantName { get; set; } = "NA";
+        /// <summary>
+        /// Document say not used in HK FPS, default "HK"
+        /// </summary>
+        public string MerchantCity { get; set; } = "HK";
+
+        public enum InitiationType
+        {
+            /// <summary>
+            /// Same QR Code is shown for more than one transaction
+            /// </summary>
+            Static,
+            /// <summary>
+            /// New QR Code is shown for each transaction
+            /// </summary>
+            Dynamic
+        }
+        public InitiationType InitiationMethod { get; set; } = InitiationType.Dynamic;
+
+        /// <summary>
+        /// Currency Code, visit <see cref="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</see> for details
+        /// </summary>
+        public string Currency { get; set; } = "344";
+        /// <summary>
+        /// Price, if assigned zero or below zero, means need input by payer himself 
+        /// </summary>
+        public decimal TransactionAmount { get; set; } = 0M;
+        /// <summary>
+        /// True to allow edit the amount by payer
+        /// </summary>
+        public bool TransactionAmountEditable { get; set; } = false;
+        /// <summary>
+        /// Document marked this is a optional field, but HSBC support say this MUST provide a unique value like invoice number as a reference
+        /// </summary>
+        public string ReferenceLabel { get; set; }
+
+        public override string ToString()
+        {
+            return Builder.GetCode(this);
+        }
+    }
+}
