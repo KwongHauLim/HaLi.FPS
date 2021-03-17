c1b92f52f56e3d01cd42ff3cbd37e0eb5386cdca
Kwong Hau Lim <haulim.kwong@gmail.com>
2021-03-17 23:12:35 +0800

    comment

--- HaLi.FPS/Sample/Program.cs
+++ HaLi.FPS/Sample/Program.cs
@@ -10,7 +10,7 @@ namespace Sample
             // Create FPS object
             var fps = new FPS
             {
-                FpsID = "1234567", // Current is 7 digits, further will 10 digits
+                FpsID = "1234567", // Current is 7 digits, further will be 10 digits
                 BankCode = "004",  // HSBC
                 InitiationMethod = FPS.InitiationType.Dynamic, // mean the payment is one time only
                 TransactionAmount = 1.00M, // $1
