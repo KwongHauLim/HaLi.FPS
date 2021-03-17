b21e0d36d1a2718996690895ac7c2a45a344db12
Kwong Hau Lim <haulim.kwong@gmail.com>
2021-03-17 23:07:50 +0800

    Sample

---
+++ HaLi.FPS/Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using HaLi.FPS;
+using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create FPS object
+            var fps = new FPS
+            {
+                FpsID = "1234567", // Current is 7 digits, further will 10 digits
+                BankCode = "004",  // HSBC
+                InitiationMethod = FPS.InitiationType.Dynamic, // mean the payment is one time only
+                TransactionAmount = 1.00M, // $1
+                TransactionAmountEditable = false, // cannot edit by payer
+                ReferenceLabel = "PayRef00001" // payment reference, HSBC is must provide the field
+            };
+            var qrCode = fps.ToString(); // QR Code content
+            // ... Generate real QR Code image by other library
+            Console.WriteLine(qrCode);
+            Console.ReadKey();
+        }
+    }
+}
